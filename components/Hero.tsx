174bc7d5fa74aa1d49658b91a1f68631d3a6c900
obedizebu <obed.izebu@yahoo.com>
2024-05-30 00:12:58 +0100

    next js website portfolio

---
+++ components/Hero.tsx
@@ -0,0 +1,56 @@
+
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/Textgenerateeffect";
+import { IoMdArrowForward } from "react-icons/io";
+
+export default function Hero() {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className=" -top-40 -left-10
+          md:-left-32 Md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className=" top-18 left-full
+          h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight
+          className=" top-20 left-80
+          h-[80vh] w-[50vw]"
+          fill="blue"
+        />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      
+    </div>
+    <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex
+        flex-col items-center justify-center">
+            <h2 className="uppercase trackin-widest text-xs text-center text-blue-100 max-w-80">
+                Dynamic web magic with next js
+            </h2>
+              <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" 
+              words="Transforming Concepts and Ideas into reality" />
+
+              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2lg">
+              Hi, I&apos;m Mark a next js developer base in Germany </p>
+
+              <a href=""> 
+                <MagicButton title="Show my work" icon={<IoMdArrowForward />} 
+                position="right"/>
+              </a>
+        </div>
+
+    </div>
+     
+    </div>
+
+    
+  );
+}
