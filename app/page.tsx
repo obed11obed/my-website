5d54ccc573484656d0f362d6f180d90cfc8e2109
obedizebu <obed.izebu@yahoo.com>
2024-05-31 13:58:00 +0100

    done with second level

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
@@ -14,6 +15,7 @@ export default function Home() {
         }/>
        <Hero />
        <Grid />
+       <RecentProject/>
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,32 +25,37 @@ export default function Hero() {
         />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      
-    </div>
-    <div className="flex justify-center relative my-20 z-10">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex
         flex-col items-center justify-center">
-            <h2 className="uppercase trackin-widest text-xs text-center text-blue-100 max-w-80">
-                Dynamic web magic with next js
-            </h2>
-              <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" 
-              words="Transforming Concepts and Ideas into reality" />
+          <h2 className="uppercase trackin-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic web magic with next js
+          </h2>
+          <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts and Ideas into reality" />
 
-              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2lg">
-              Hi, I&apos;m Mark a next js developer base in Germany </p>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2lg">
+            Hi, I&apos;m Mark a next js developer base in Germany </p>
 
-              <a href=""> 
-                <MagicButton title="Show my work" icon={<IoMdArrowForward />} 
-                position="right"/>
-              </a>
+          <a href="">
+            <MagicButton title="Show my work" icon={<IoMdArrowForward />}
+              position="right" />
+          </a>
         </div>
 
-    </div>
-     
+      </div>
+
+
+
+
+
+
     </div>
 
-    
+
   );
 }
